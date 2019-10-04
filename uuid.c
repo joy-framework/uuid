247bbebf6182963ef8e9048e89ca62aaf10f1c54
Sean Walker <swlkr.rbl@gmail.com>
2019-10-03 20:24:23 -0700

    Use a char array instead of malloc

--- uuid.c
+++ uuid.c
@@ -8,23 +8,13 @@ static Janet generate(int32_t argc, const Janet *argv) {
 
   uuid_t binuuid;
 
-  /*
-   * Generate a UUID. We're not done yet, though,
-   * for the UUID generated is in binary format
-   * (hence the variable name). We must 'unparse'
-   * binuuid to get a usable 36-character string.
-   */
   uuid_generate_random(binuuid);
 
-  /*
-   * uuid_unparse() doesn't allocate memory for itself, so do that with
-   * malloc(). 37 is the length of a UUID (36 characters), plus '\0'.
-   */
-  char *uuid = malloc(36);
+  char uuid[36];
 
   uuid_unparse_upper(binuuid, uuid);
 
-  return janet_stringv((const uint8_t *) uuid, 36);
+  return janet_stringv((const uint8_t *)uuid, 36);
 }
 
 static const JanetReg cfuns[] = {
